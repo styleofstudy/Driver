991b3ce3f6b65b788679ff43d9b4b46bfa7f9a8d
金果_刘学峰 <liuxuefeng@doowintech.com>
2023-04-25 16:55:06 +0800

    summary:commit-v1.0.1

--- driver/src/main/java/com/xueng/driver/toast/interfaces/ToastGravityFactory.kt
+++ driver/src/main/java/com/xueng/driver/toast/interfaces/ToastGravityFactory.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.me.codelib.toast.interfaces
+package com.xueng.driver.toast.interfaces
 
 import android.content.Context
 import android.view.Gravity
